2d7414c077f9c4df5ca02249be34120d3b0d2724
Urmil Parikh <urmil@klarsys.com>
2015-01-12 19:48:36 +0530

    Update

--- demo.js
+++ demo.js
@@ -1,14 +1,14 @@
 angular.module('demoapp', ['ngMdIcons'])
 	.controller('democtrl', function($scope) {
-		var icons = ['home', 'alarm', 'favorite', 'help', 'shop', 'call', 'email', 'send', 'storage', 'usb', 'flight', 'share'];
-		var colors = ['#abcdef', 'aqua', 'deepskyblue', 'yellow', 'springgreen', 'lightgreen', 'orchid', 'goldenrod', 'wheat', '#cc99cc'];
+		var icons = ['home', 'sd_card', 'favorite', 'help', 'shop', 'call', 'email', 'settings', 'car', 'flight', 'share'];
+		var colors = ['lightgreen', 'pink', 'wheat', '#cc99ff', '#abcdef'];
 		$scope.icon = icons[0];
 		$scope.fill = colors[0];
 		$scope.size = 36;
 
 		setInterval(function() {
 			var random = Math.random();
-			if (random < 0.3)
+			if (random < 0.5)
 				$scope.icon = icons[Math.floor(Math.random() * icons.length)];
 			else if (random < 0.7)
 				$scope.fill = colors[Math.floor(Math.random() * colors.length)];
