11634846a0341dbed5dd956d970ac9147321e8cc
Urmil Parikh <urmil@klarsys.com>
2015-08-18 10:37:22 +0530

    Examples of adding custom icons (#30)

--- demo.js
+++ demo.js
@@ -31,4 +31,13 @@ angular.module('demoapp', ['ngMdIcons'])
             }
             $scope.$apply();
         }, 1700);
+    })
+    .config(function(ngMdIconServiceProvider) {
+        ngMdIconServiceProvider
+            .addShape('custom-bookmark', '<path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>')
+            // .addShape('custom-delete', ngMdIconService.getShape('delete'))
+            .addShapes({
+                'custom-done': '<path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>',
+                'custom-home': '<path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>'
+            });
     });
