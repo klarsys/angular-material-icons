4090c2cd53328e53ad615d98a0f8fa26d3105d71
Urmil Parikh <urmil@klarsys.com>
2015-09-15 10:51:08 +0530

    Add gulp task for minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
     $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('lint', function() {
@@ -12,6 +13,15 @@ gulp.task('lint', function() {
         .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('minify', function() {
+    return gulp.src([
+        'angular-material-icons.js'
+    ])
+        .pipe($.uglify())
+        .pipe($.rename('angular-material-icons.min.js'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['lint', 'minify']);
 
 module.exports = gulp;
